e2d627a0a15de56d7d8711bc28a4a62d708d0ad9
Joonho Wohn <givenarc@gmail.com>
2022-10-07 17:14:33 +0900

    add files

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
 #docker build -t cu11_nn8_1804 cuda_11_cudnn8_ubuntu1804 
-docker build -t cu11-4_nn8_1804 cuda_11-4_cudnn8_ubuntu1804 
+#docker build -t cu11-4_nn8_1804 cuda_11-4_cudnn8_ubuntu1804 
+docker build -t l4t_tf_291 jetson_r35_5-0-2-tf
 
